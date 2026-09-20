0ca749f429631ca46d3bf5d8000b0811030bfa94
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2026-09-20 12:10:34 -0400

    Reintroduced MessagePacked (non-generic) so that a downstream implicit conversion operator can be created.

--- NorthSouthSystems.MessagePackable/MessagePacked.cs
+++ NorthSouthSystems.MessagePackable/MessagePacked.cs
@@ -3,9 +3,26 @@ using System.IO.Hashing;
 
 namespace NorthSouthSystems.MessagePackable;
 
-public class MessagePacked<T> where T : IMessagePackable, IShapeable<T>
+public class MessagePacked<T> : MessagePacked
+    where T : IMessagePackable, IShapeable<T>
 {
-    public MessagePacked(ImmutableArray<byte> bytes)
+    public MessagePacked(ImmutableArray<byte> bytes) : base(bytes) { }
+
+    public T ToT(CancellationToken token = default) =>
+        T.MessagePack.Deserialize<T>(Bytes.AsMemory(), token)!;
+
+    public void RoundTripThrowIfDiff(CancellationToken token = default)
+    {
+        var roundTrip = ToT(token).ToMessagePacked(token);
+
+        if (XxHash128 != roundTrip.XxHash128)
+            throw new ArgumentException("MessagePack round-trip failure.");
+    }
+}
+
+public class MessagePacked
+{
+    protected private MessagePacked(ImmutableArray<byte> bytes)
     {
         if (bytes.IsDefault)
             throw new ArgumentException("Cannot be default ImmutableArray.", nameof(bytes));
@@ -19,15 +36,4 @@ public class MessagePacked<T> where T : IMessagePackable, IShapeable<T>
 
     public ImmutableArray<byte> Bytes { get; }
     public UInt128 XxHash128 { get; }
-
-    public T ToT(CancellationToken token = default) =>
-        T.MessagePack.Deserialize<T>(Bytes.AsMemory(), token)!;
-
-    public void RoundTripThrowIfDiff(CancellationToken token = default)
-    {
-        var roundTrip = ToT(token).ToMessagePacked(token);
-
-        if (XxHash128 != roundTrip.XxHash128)
-            throw new ArgumentException("MessagePack round-trip failure.");
-    }
 }
